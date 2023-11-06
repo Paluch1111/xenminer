5d254db608e6e87953ddb2641e159d9b4a7ed77d
Paluch1111 <146782740+Paluch1111@users.noreply.github.com>
2023-11-06 21:53:42 +0100

    gpu2.sh

--- gpu2.sh
+++ gpu2.sh
@@ -15,7 +15,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x0a6969fff003b760c97005e03ff5a9741126167a/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xcbFe57DDaCef92B2Cc0772f2D8948B0697b7E07a/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
